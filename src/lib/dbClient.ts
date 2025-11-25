81dd57758710071b44193b033ae64b2769db500e
Saikat Sardar <saikatsardar5@gmail.com>
2025-11-25 15:19:01 +0530

    Update dbClient.ts

--- src/lib/dbClient.ts
+++ src/lib/dbClient.ts
@@ -2,15 +2,15 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../../generated/prisma/client";
 import { serverEnv } from "./env/serverEnv";
 
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+	prisma?: PrismaClient;
 };
 
 const adapter = new PrismaBetterSqlite3({
 	url: serverEnv.DATABASE_URL,
 });
 
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
 	globalForPrisma.prisma = prisma;
