d3f8a388ab86cbe8549d11b60bc034cc4a5ccf53
Saikat Sardar <saikatsardar5@gmail.com>
2026-07-25 17:07:03 +0530

    init(v2): fresh

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-import "./src/lib/env/clientEnv";
-import "./src/lib/env/serverEnv";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  typedRoutes: true,
 };
 
 export default nextConfig;
